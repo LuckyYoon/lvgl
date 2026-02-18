8ca7a2cbd361e2a9f74b07670f13880c54d9c34d
LuckyYoon <justinyoon1234@gmail.com>
2026-02-17 20:42:10 -0500

    REVERTED AGAIN

--- src/font/lv_font.h
+++ src/font/lv_font.h
@@ -337,33 +337,33 @@ LV_FONT_DECLARE(lv_font_unscii_8)
 LV_FONT_DECLARE(lv_font_unscii_16)
 #endif
 
-#if LV_FONT_ROBOTO_24
-LV_FONT_DECLARE(lv_font_roboto_24)
-#endif
-
-#if LV_FONT_ROBOTO_32
-LV_FONT_DECLARE(lv_font_roboto_32)
-#endif
-
-#if LV_FONT_ROBOTO_40
-LV_FONT_DECLARE(lv_font_roboto_40)
-#endif
-
-#if LV_FONT_ROBOTO_48
-LV_FONT_DECLARE(lv_font_roboto_48)
-#endif
-
-#if LV_FONT_ROBOTO_64
-LV_FONT_DECLARE(lv_font_roboto_64)
-#endif
-
-#if LV_FONT_ROBOTO_184
-LV_FONT_DECLARE(lv_font_roboto_184)
-#endif
-
 /*Declare the custom (user defined) fonts*/
 #ifdef LV_FONT_CUSTOM_DECLARE
 LV_FONT_CUSTOM_DECLARE
+#endif
+
+#if LV_FONT_ROBOTO_24
+LV_FONT_CUSTOM_DECLARE(lv_font_roboto_24)
+#endif
+
+#if LV_FONT_ROBOTO_32
+LV_FONT_CUSTOM_DECLARE(lv_font_roboto_32)
+#endif
+
+#if LV_FONT_ROBOTO_40
+LV_FONT_CUSTOM_DECLARE(lv_font_roboto_40)
+#endif
+
+#if LV_FONT_ROBOTO_48
+LV_FONT_CUSTOM_DECLARE(lv_font_roboto_48)
+#endif
+
+#if LV_FONT_ROBOTO_64
+LV_FONT_CUSTOM_DECLARE(lv_font_roboto_64)
+#endif
+
+#if LV_FONT_ROBOTO_184
+LV_FONT_CUSTOM_DECLARE(lv_font_roboto_184)
 #endif
 
 #ifdef __cplusplus
